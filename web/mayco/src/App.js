f78e532a00a55f413ada52cdbd94698b5a6f0770
Antonio Oviedo <antoniooviedo131102@gmail.com>
2024-07-11 12:50:59 -0600

    Base

---
+++ web/mayco/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component { 
+  componentDidMount() {
+    document.title = "Mayco";
+  } 
+  render() { 
+    return ( 
+      <div>
+        
+      </div> 
+    ); 
+  } 
+} 
+
+export default App; 
